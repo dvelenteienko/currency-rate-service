e23782dc0d01e6245a04f28d4f645959372959ad
dvelenteienko <dimonwilli@gmail.com>
2024-02-29 18:18:53 +0200

    Push project

---
+++ src/test/java/com/dvelenteienko/services/currency/CurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dvelenteienko.services.currency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyApplicationTests {
+
+//	@Test
+	void contextLoads() {
+	}
+
+}
