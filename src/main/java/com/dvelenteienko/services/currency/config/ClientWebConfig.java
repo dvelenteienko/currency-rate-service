8bdceaba50bf631a0e19d7a36af4a242a3fc5d95
dvelenteienko <dimonwilli@gmail.com>
2024-06-18 13:48:54 +0300

    Added cors mapping

--- src/main/java/com/dvelenteienko/services/currency/config/ClientWebConfig.java
+++ src/main/java/com/dvelenteienko/services/currency/config/ClientWebConfig.java
@@ -3,14 +3,25 @@ package com.dvelenteienko.services.currency.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class ClientWebConfig implements WebMvcConfigurer {
 
     @Bean
     public MethodValidationPostProcessor methodValidationPostProcessor() {
         return new MethodValidationPostProcessor();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/v1/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS")
+                .allowedHeaders("Access-Control-Allow-Origin")
+                .allowCredentials(false).maxAge(3600);
     }
 }
