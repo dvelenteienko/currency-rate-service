550c63d93c6c99cca25cbb9268913d1160a52a6c
dvelenteienko <dimonwilli@gmail.com>
2024-06-29 23:47:23 +0300

    Change auth data for relevant level of cost factor init admin pwd

--- src/main/java/com/dvelenteienko/services/currency/config/SecurityConfig.java
+++ src/main/java/com/dvelenteienko/services/currency/config/SecurityConfig.java
@@ -18,11 +18,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -88,7 +83,7 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(12);
     }
 
     @Bean
--- src/main/resources/db/changelog/changes/1.0.3-insert_initial_users_and_roles.sql
+++ src/main/resources/db/changelog/changes/1.0.3-insert_initial_users_and_roles.sql
@@ -7,7 +7,8 @@ VALUES ('3f6482bd-f61d-4561-b5f4-8ff69f691402', 'ADMIN');
 
 -- Insert some sample data into User table
 INSERT INTO user_account (id, username, password)
-VALUES ('1b531074-5a6d-409d-b166-e007ceac301a', 'admin', 'pwd');
+-- init pwd admin
+VALUES ('1b531074-5a6d-409d-b166-e007ceac301a', 'admin', '$2y$12$hNu71GcqkqjpvigNjrPK0uaqtWvn2iARA3YjgXiJHgqGJJSpgJkNa');
 
 -- Assign roles to users
 INSERT INTO users_roles (user_id, role_id)
