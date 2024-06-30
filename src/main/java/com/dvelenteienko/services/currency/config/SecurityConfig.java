1bb0c028a6d8900d167cdf2446128f52aa9cfdc3
dvelenteienko <dimonwilli@gmail.com>
2024-06-30 10:58:48 +0300

    Removed cors config for a while

--- src/main/java/com/dvelenteienko/services/currency/config/SecurityConfig.java
+++ src/main/java/com/dvelenteienko/services/currency/config/SecurityConfig.java
@@ -96,13 +96,4 @@ public class SecurityConfig {
         return new MethodValidationPostProcessor();
     }
 
-//    @Bean
-//    CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(Arrays.asList("https://example.com"));
-//        configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//    }
 }
